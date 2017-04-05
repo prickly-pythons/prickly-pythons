533bc91871b9f00c8c2651f18e44dadcfb81d607
kpolsen <kpolsen@asu.edu>
2017-04-04 17:01:09 -0700

    Code and presentation on submodules

--- code_from_meetings/making_modules/structure_example/my_module_name/submodule1.py
+++ code_from_meetings/making_modules/structure_example/my_module_name/submodule1.py
@@ -4,9 +4,14 @@ print('This is sub-module 1')
 def function1():
 	print('This is function 1')
 
-	number1        	=    float(raw_input('write one number: [default: 1] ... '))
-	number2        	=    float(raw_input('write another number: [default: 1] ... '))
-    
+	number1        	=    raw_input('write one number: [default: 1] ... ')
+	if number1 == '': number1 = '1'
+	number2        	=    raw_input('write another number: [default: 1] ... ')
+	if number2 == '': number2 = '1'
+
+	number1 		=	float(number1)
+	number2 		=	float(number2)
+
 	print('The sum of these two numbers is: '+str(number1+number2))
 
 	# print('The difference between these two numbers is: '+str(number1-number2))
